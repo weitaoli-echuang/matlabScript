e8a12d78f09cb463d9e0b5529011b04c487252cf
lwt <weitaoli.echung@gmail.com>
2016-02-17 13:42:27 +0800

    modify

--- load_mesh_test.m
+++ load_mesh_test.m
@@ -7,12 +7,4 @@ obj_file_1 = 'D:\\Users\\Desktop\\lwtRelative\\PoissonRecon.x64\\PoissonRecon.x6
 obj_file_2 = 'D:\\Users\\Desktop\\lwtRelative\\PoissonRecon.x64\\PoissonRecon.x64\\Possion_leftleg\\outlgdr1.obj';
 [vertices_2,vertices_normal_2,faces_2]=load_mesh(obj_file_2, 'obj');
 
-x_range = max(vertices(:,1)) - min(vertices(:,1));
-y_range = max(vertices(:,2)) - min(vertices(:,2));
-z_range = max(vertices(:,3)) - min(vertices(:,3));
-
-center_x = x_range / 2.;
-center_y = y_range / 2.;
-center_z = z_range / 2.;
-
 center = vertices_2 - vertices_1;
