0f1be25d0ee54166ab7b96e7484343499b520e08
lwt <weitaoli.echung@gmail.com>
2016-02-18 14:38:53 +0800

    clean script and reform some function

--- Untitled.m
+++
@@ -1,8 +0,0 @@
-Z= peaks; surf(Z)
-axis tight
-set(gca,'nextplot','replacechildren');
-for j = 1:20
-    surf(sin(2*pi*j/20)*Z,Z)
-    F(j) = getframe;
-end
-movie(F,20) % Play the movie twenty times
--- reorient_vertices_normal.m
+++
@@ -1,7 +0,0 @@
-
-function reorient_vertices_normal()
-end
-
-function read_ply_file(path)
-
-end
